21b6b3d4156befbd5a9dedc8cd69a67063acfdf0
Codigo Aldea <dmdndrd@gmail.com>
2026-05-01 08:37:40 +0530

    ape-confige updated

--- token_app_custom_defi/contracts/Token.sol
+++ token_app_custom_defi/contracts/Token.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import "/lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
 contract Token{
     string public name;
     string public symbol;
